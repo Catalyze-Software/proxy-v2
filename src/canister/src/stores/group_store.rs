0cb63495cb6b8455d42717b2a28d7436a1d23716
Remco <remco@r3m.codes>
2024-01-12 20:19:17 +0100

    Group methods

--- src/canister/src/stores/group_store.rs
+++ src/canister/src/stores/group_store.rs
@@ -3,7 +3,10 @@ use std::cell::{Ref, RefCell, RefMut};
 use ic_stable_structures::StableBTreeMap;
 
 use crate::{
-    entities::{group::Group, member::Member},
+    entities::{
+        group::{Group, PostGroup},
+        member::Member,
+    },
     stores::memory_store::{MemManager, MEMORY_MANAGER},
 };
 
@@ -32,16 +35,12 @@ thread_local! {
     );
 }
 
-pub trait GroupManager {
+pub trait GroupStoreMethods {
     fn groups(&self) -> GroupStore;
     fn groups_mut(&mut self) -> MutableGroupStore;
-    // fn get(&self, key: &u64) -> Option<Group>;
-    // fn add(&self, value: Group);
-    // fn update(&self, key: &u64, value: Group);
-    // fn remove(&self, key: &u64);
 }
 
-impl GroupManager for GroupStoreRef {
+impl GroupStoreMethods for GroupStoreRef {
     fn groups(&self) -> GroupStore {
         self.borrow()
     }
@@ -50,46 +49,3 @@ impl GroupManager for GroupStoreRef {
         self.borrow_mut()
     }
 }
-
-// //// OLD FOR REFERERNCE
-// /// The `Groups` struct.
-// /// # Note
-// /// This struct is used to interact with the `groups` store.
-// pub struct Groups;
-
-// impl Groups {
-//     /// Returns the `group` associated with the given key.
-//     /// # Arguments
-//     /// * `key` - The key to get the `group` at in `u64` format.
-//     /// # Returns
-//     /// * `Some(Group)` - The `group` associated with the given key.
-//     /// * `None` - If no `group` is associated with the given key.
-//     /// # Panics
-//     /// Panics if the `group` associated with the given key is not a `group`.
-//     pub fn get(key: &u64) -> Option<Group> {
-//         GROUPS.with(|p| p.borrow().get(key))
-//     }
-
-//     /// Inserts or updates a `group` into the store at a given key's position.
-//     /// # Arguments
-//     /// * `key` - The key to insert the `group` at in `u64` format.
-//     /// * `value` - The `group` to insert.
-//     /// # Panics
-//     /// Panics if the `group` associated with the given key is not a `group`.
-//     /// # Note
-//     /// This function will overwrite the `group` at the given key's position if one already exists.
-//     pub fn insert(key: u64, value: Group) {
-//         GROUPS.with(|p| p.borrow_mut().insert(key, value));
-//     }
-
-//     /// Removes a `group` from the store at a given key's position.
-//     /// # Arguments
-//     /// * `key` - The key to insert the `group` at in `u64` format.
-//     /// # Panics
-//     /// Panics if the `group` associated with the given key is not a `group`.
-//     /// # Note
-//     /// This function will do nothing if no `group` is associated with the given key.
-//     pub fn remove(key: &u64) {
-//         GROUPS.with(|p| p.borrow_mut().remove(key));
-//     }
-// }
