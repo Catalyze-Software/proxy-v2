2f98604b36890b354285c0377d671d142a285ab2
Web3NL <samerweb3@gmail.com>
2024-02-13 14:41:45 +0100

    wip import proxy crate

--- src/test/src/lib.rs
+++ src/test/src/lib.rs
@@ -1,25 +1,33 @@
-use candid::{self, encode_one, Principal};
-use pocket_ic::{self, PocketIc, WasmResult};
+use candid::{encode_one, Principal};
+use canister::models::boosted::Boosted;
 use ic_cdk::api::management_canister::main::CanisterId;
+use pocket_ic::{self, PocketIc, WasmResult};
 
-
- #[test]
- fn test_proxy() {
+#[test]
+fn test_proxy() {
     let pic = PocketIc::new();
 
     // Create an empty canister as the anonymous principal.
     let canister_id = pic.create_canister();
     pic.add_cycles(canister_id, 2_000_000_000_000); // 2T cycles
 
-    let wasm_bytes = std::fs::read("../../wasm/canister.wasm.gz").expect("Failed to read wasm file");
+    let wasm_bytes =
+        std::fs::read("../../wasm/canister.wasm.gz").expect("Failed to read wasm file");
     pic.install_canister(canister_id, wasm_bytes, vec![], None);
 
-    let reply = call(&pic, canister_id, "get_boosted_groups");
-    // assert_eq!(reply, WasmResult::Reply(vec![0]));
-    println!("{:?}", reply);
- }
+    let WasmResult::Reply(bytes) = call(&pic, canister_id, "get_boosted_groups");
+
+    println!("\n\n{:?}\n\n", &bytes);
+
+    let expected: Vec<Boosted> = candid::decode_one::<Vec<Boosted>>(&bytes).unwrap();
+}
 
 fn call(pic: &PocketIc, canister_id: CanisterId, method: &str) -> WasmResult {
-    pic.update_call(canister_id, Principal::anonymous(), method, encode_one(()).unwrap())
-        .expect("Failed to call counter canister")
+    pic.update_call(
+        canister_id,
+        Principal::anonymous(),
+        method,
+        encode_one(()).unwrap(),
+    )
+    .expect("Failed to call counter canister")
 }
