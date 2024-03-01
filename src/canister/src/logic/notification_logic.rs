ea56dc01a349a3869104bb6debb1c01db058ddeb
Remco <remco@r3m.codes>
2024-03-01 15:50:42 +0100

    Remove stuff

--- src/canister/src/logic/notification_logic.rs
+++ src/canister/src/logic/notification_logic.rs
@@ -1,8 +1,3 @@
-use candid::Principal;
-use canister_types::models::notification::NotificationType;
-
 pub struct NotificationCalls;
 
-impl NotificationCalls {
-    pub fn new(to: Vec<Principal>, silent: bool, notification_type: NotificationType) -> () {}
-}
+impl NotificationCalls {}
