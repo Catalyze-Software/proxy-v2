bfb7580a543780df536d01edfbc16e02427f5261
Semen Loktionov <loktionov.kh@gmail.com>
2024-07-01 17:00:09 +0300

    fix candid types generation

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,5 +9,7 @@ cargo build -p proxy --release --target wasm32-unknown-unknown
 # Gzip wasm
 gzip -c target/wasm32-unknown-unknown/release/proxy.wasm > target/wasm32-unknown-unknown/release/proxy.wasm.gz
 
+mkdir -p wasm
+
 # Copy wasm
 cp target/wasm32-unknown-unknown/release/proxy.wasm.gz wasm/proxy.wasm.gz
