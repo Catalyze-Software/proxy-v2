5ee28cfa2eb3b124fe2d5391ee730ace35e7627e
Remco <remco@r3m.codes>
2024-09-02 10:23:53 +0200

    Resolve formatting error

--- src/proxy/src/storage/cells.rs
+++ src/proxy/src/storage/cells.rs
@@ -1,9 +1,12 @@
 use candid::Principal;
 use catalyze_shared::{CellStorage, GenericCellStorage};
 
-use super::{storage_api::{
-    GLOBAL_CANISTER, HISTORY_CANISTER, PROFILE_CANISTER, REPORT_CANISTER, TOPIC_CANISTER,
-}, TRANSACTION_HANDLER_CANISTER};
+use super::{
+    storage_api::{
+        GLOBAL_CANISTER, HISTORY_CANISTER, PROFILE_CANISTER, REPORT_CANISTER, TOPIC_CANISTER,
+    },
+    TRANSACTION_HANDLER_CANISTER,
+};
 
 pub fn history_canister() -> impl CellStorage<Principal> {
     GenericCellStorage::new("history_canister", &HISTORY_CANISTER)
@@ -26,5 +29,8 @@ pub fn global_canister() -> impl CellStorage<Principal> {
 }
 
 pub fn transaction_handler_canister() -> impl CellStorage<Principal> {
-    GenericCellStorage::new("transaction_handler_canister_id", &TRANSACTION_HANDLER_CANISTER)
+    GenericCellStorage::new(
+        "transaction_handler_canister_id",
+        &TRANSACTION_HANDLER_CANISTER,
+    )
 }
